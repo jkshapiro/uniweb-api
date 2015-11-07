65a268f2bc5f4e09a2072d15cc33e91bd232eae2
Diego Macrini <dmacrini@gmail.com>
2015-11-06 20:22:46 -0500

    Fixed spacing

--- Examples/example1.php
+++ Examples/example1.php
@@ -4,9 +4,13 @@ require_once('client.php');
 $client = getClient();
 $filter = array('unit' => 'Engineering', 'title' => 'Professor'); 
 
-$resources = array('profile/membership_information', 
-	'profile/research_interests', 'profile/research_description');
- 	$params = array('resources' => $resources, 'filter' => $filter);
+$resources = array(
+	'profile/membership_information', 
+	'profile/research_interests', 
+	'profile/research_description'
+);
+	
+$params = array('resources' => $resources, 'filter' => $filter);
 
 $result = $client->read($params);
 var_dump($result);
